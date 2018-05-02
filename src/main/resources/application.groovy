e369928bc6422e112f3f9757169aba6a915a287a
Jeff Scott Brown <brownj@objectcomputing.com>
2018-05-01 19:30:15 -0500

    Add Postgres support for GCP enfv
    This is not the best way to manage the credentials but is a quick way
    to get it operational for now.

--- src/main/resources/application.groovy
+++ src/main/resources/application.groovy
@@ -2,6 +2,14 @@ micronaut.application.name='microservicepoc'
 micronaut.server.port=8080
 hibernate {
     hbm2ddl {
-        auto = 'create-drop'
+        auto = 'update'
+    }
+}
+
+if(System.getenv('PG_USERNAME')) {
+    dataSource {
+        url = 'jdbc:postgresql://google/people?socketFactory=com.google.cloud.sql.postgres.SocketFactory&socketFactoryArg=microservice-poc-202623:us-central1:postgres'
+        username = System.getenv('PG_USERNAME')
+        password = System.getenv('PG_PASSWORD')
     }
 }
