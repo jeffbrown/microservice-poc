ef6fce963feb2c7a1c4e848fff685138a9abfb65
Jeff Scott Brown <brownj@objectcomputing.com>
2018-05-14 19:46:27 -0500

    Use GCP Metadata Server

--- src/main/groovy/people/email/sendgrid/SendGridEmailBeanFactory.groovy
+++ src/main/groovy/people/email/sendgrid/SendGridEmailBeanFactory.groovy
@@ -12,7 +12,7 @@ class SendGridEmailBeanFactory {
 
     @Singleton
     SendGrid sendGrid() {
-        final String sendgridApiKey = System.getenv 'SENDGRID_API_KEY'
+        final String sendgridApiKey = 'http://metadata.google.internal/computeMetadata/v1/project/attributes/SENDGRID_API_KEY'.toURL().getText(requestProperties: ['Metadata-Flavor': 'Google'])
         new SendGrid(sendgridApiKey)
     }
 }
--- src/main/resources/application-gcp.groovy
+++ src/main/resources/application-gcp.groovy
@@ -1,6 +1,8 @@
 dataSource {
-    url = 'jdbc:postgresql://google/people?socketFactory=com.google.cloud.sql.postgres.SocketFactory&socketFactoryArg=microservice-poc-202623:us-central1:postgres'
-    username = System.getenv('PG_USERNAME')
-    password = System.getenv('PG_PASSWORD')
+    String metadataUrlRoot = 'http://metadata.google.internal/computeMetadata/v1/project/attributes'
+    Map requestHeaders = [requestProperties: ['Metadata-Flavor': 'Google']]
+    url      = "${metadataUrlRoot}/PG_JDBC_URL".toURL().getText(requestHeaders)
+    username = "${metadataUrlRoot}/PG_USERNAME".toURL().getText(requestHeaders)
+    password = "${metadataUrlRoot}/PG_PASSWORD".toURL().getText(requestHeaders)
 }
 
