9002b341d088b996284cd672cb65cb100594c85f
sdelamo <sergio.delamo@softamo.com>
2018-08-21 09:34:14 +0200

    implement GormEntity in Person

--- src/main/groovy/people/Person.groovy
+++ src/main/groovy/people/Person.groovy
@@ -1,9 +1,10 @@
 package people
 
 import grails.gorm.annotation.Entity
+import org.grails.datastore.gorm.GormEntity
 
 @Entity
-class Person {
+class Person implements GormEntity<Person> {
     String firstName
     String lastName
     int age
